4bdff969a7655523c25d57709d95e80b117ab3cb
Vovan <volodiakyshenia2@gmail.com>
2022-05-16 15:43:55 +0300

    Other

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $(this).click(function(){
+    $("#sort").click(function(){
+        let kindOfSort = $("#kindOfSort").val();
+        let sizeOfArray = $("#sizeOfArray").val();
+        $.ajax({
+            type: "GET",
+            url: "index.php",
+            data: {kindOfSort, sizeOfArray}
+        });
+    });
+
+    $("#file").click(function(){
+        let kindOfSort = $("#kindOfSort").val();
+        let sizeOfArray = $("#sizeOfArray").val();
+        $.ajax({
+            type: "GET",
+            url: "index.php",
+            data: {kindOfSort, sizeOfArray}
+        });
+    });
+
+    $("#db").click(function(){
         let kindOfSort = $("#kindOfSort").val();
         let sizeOfArray = $("#sizeOfArray").val();
         $.ajax({
