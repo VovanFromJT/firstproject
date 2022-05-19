0197d74f1d4675b6f2202260d239c42c61d3aca8
Vovan <volodiakyshenia2@gmail.com>
2022-05-19 20:50:22 +0300

    Other

---
+++ src/Output/WriterFile.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Source\Output;
+
+use Exception;
+
+class WriterFile extends Write
+{
+
+    public function __construct(
+        array $outputArray,
+        int $sizeOfArray,
+        string $name,
+        array $inputArray
+    ) {
+        parent::__construct(
+            $outputArray,
+            $sizeOfArray,
+            $name,
+            $inputArray
+        );
+    }
+
+    public function outputArray(): void {
+        try {
+            $file = fopen(
+                "files/txt/" . $this->name . ".txt",
+                "w"
+            );
+            for ($firstIndex = 0; $firstIndex < $this->sizeOfArray; $firstIndex++) {
+                for ($secondIndex = 0; $secondIndex < $this->sizeOfArray; $secondIndex++) {
+                    fwrite($file, $this->outputArray[$firstIndex][$secondIndex] . "\t");
+                }
+                fwrite($file, "\n");
+            }
+            fclose($file);
+            echo "<p>files/txt/" . $this->name . ".txt was successfully created:)</p>";
+        }
+        catch (Exception $e) {
+            echo "Message: " . $e->getMessage();
+        }
+    }
+}
